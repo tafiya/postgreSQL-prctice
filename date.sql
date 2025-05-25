d30f5a386fe34483208d303be778b1df9d56c58f
tafiya <tafiapinkey@email.com>
2025-05-26 01:52:37 +0600

    date related query

---
+++ date.sql
@@ -0,0 +1,31 @@
+SHOW timezone;
+SELECT now()
+
+create Table timez(
+    ts TIMESTAMP without TIME zone,
+    tsz TIMESTAMP with TIME zone 
+);
+INSERT into timez VALUES ('2024-01-8 10:30:00','2024-01-8 10:30:00')
+
+SELECT * from timez;
+
+--  Shudu date show ar jonno date function
+ SELECT CURRENT_DATE;
+ SELECT now()::date;
+ SELECT now()::TIME;
+
+--   to__char function
+SELECT to_char(now(),'DDD');
+
+-- interval
+SELECT CURRENT_DATE -INTERVAL '1 year 2 month';
+
+-- age function diye age related sob kisu ber kora jay
+ SELECT age( CURRENT_DATE,'1999-01-01');
+
+SELECT *, age(CURRENT_DATE,dob) as Years from students ;
+
+-- extract korar jonno use hoy
+SELECT extract( month from '2024-01-25'::date);
+
+-- type casting 
